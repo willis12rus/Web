5541819f8ad836aec4192fde8b585ca6b0f4b599
willis12rus <49200813+willis12rus@users.noreply.github.com>
2020-06-11 12:13:02 +0300

    add lw2

---
+++ lab2/Print Args/PrintArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Print_Args
+{
+    class CommandLine
+    {
+        public static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No parameters were specified.");
+                return 1;
+            }
+
+            Console.WriteLine($"Number of arguments: " + args.Length);
+            Console.WriteLine($"Arguments: {string.Join(" ", args)}");
+
+            return 0;
+        }
+    }
+}
---
+++ lab2/RemoveDuplicates/RemoveDuplicates.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RemoveDuplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Incorrect number of arguments!");
+                Console.WriteLine("Usage RemoveDuplicates.exe <input string>");
+                return;
+            }
+            Dictionary<char, bool> exitingChars = new Dictionary<char, bool>();
+            string line = args[0];
+            foreach (char ch in line)
+            {
+                if (!exitingChars.ContainsKey(ch))
+                {
+                    exitingChars[ch] = true;
+                    Console.Write(ch);
+                }
+            }
+        }
+    }
+}
